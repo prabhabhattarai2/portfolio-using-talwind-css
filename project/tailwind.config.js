79eb20270c637ab68c3a5690802b2611d766a422
prabhabhattarai2 <131674634+prabhabhattarai2@users.noreply.github.com>
2024-03-15 13:33:16 +0545

    portfolio-using-talwind-css

---
+++ project/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brightColor: "#CC00CC",
+        backgroundColor: "#F0F0F6"
+      },
+    },
+  },
+  plugins: [],
+};
+
